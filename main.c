cc90e7c0713e95aba3ba02a51c5bd4f2055e1752
O.Tochi <onyemstochi3@gmail.com>
2022-08-09 13:58:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,21 +28,17 @@ int main(void)
     printf("Unsigned octal:[%o]\n", ui);
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c], %c\n", 72, NULL);
-    printf("Character:[%c], %c\n", 72, NULL);
-   	int l1 = _printf("%s\n", NULL);
-    int l2 = printf("%s\n", NULL);
-	printf("custom: %d, standard: %d\n", l1, l2);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%%]\n");
-    len2 = printf("Percent:[%%%]\n");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
-    _printf("%\n");
-    printf("%\n");
-    _printf("Binary: %b\n", 98);
     return (0);
 }
